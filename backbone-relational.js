0d4c8e59bdcb8afe745217db98fcf36f756bbf5b
David Baumgold <singingwolfboy@gmail.com>
2011-12-09 18:02:07 -0500

    Added `collectionKey` option for HasMany relations
    If you set `collectionKey` to `true` on a HasMany relation (default), the relation's
    `key` attribute will be used to create a reference to the RelationalModel instance
    from the generated collection. Alternatively, if you set `collectionKey` to a
    string, it will use that string as the reference to the RelationalModel, rather
    than the relation's `key` attribute. If you set `collectionKey` to false (or any
    falsy value) this reference will not be created.

--- backbone-relational.js
+++ backbone-relational.js
@@ -598,7 +598,8 @@
 		
 		options: {
 			reverseRelation: { type: 'HasOne' },
-			collectionType: Backbone.Collection
+			collectionType: Backbone.Collection,
+			collectionKey: true
 		},
 		
 		initialize: function() {
@@ -625,6 +626,14 @@
 			
 			collection.reset();
 			collection.model = this.relatedModel;
+			var collectionKey = this.options.collectionKey;
+			if(collectionKey) {
+				if(collectionKey === true) {
+					collection[this.options.reverseRelation.key] = this.instance;
+				} else {
+					collection[collectionKey] = this.instance;
+				}
+			}
 			collection.bind( 'relational:add', this.handleAddition ).bind('relational:remove', this.handleRemoval );
 			return collection;
 		},
