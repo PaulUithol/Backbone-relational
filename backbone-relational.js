28dd9b3fd504df3ffdabee7db277ac64b20e4c1d
Clemens <clemens@heypanda.de>
2012-05-22 11:51:49 +0200

    fixed bug where indexes were all the same when adding models to collection
    - happens because 'add' events are added to the relational event queue,
    passing the arguments (including options) as reference.. the index, at
    which a model was added is then changed for every call to trigger('add')
    so that when the event queue is finally run, options.index does only
    reflect the last change to the index and not the index the model was
    originally added at...

--- backbone-relational.js
+++ backbone-relational.js
@@ -1606,6 +1606,21 @@
 	Backbone.Collection.prototype.trigger = function( eventName ) {
 		if ( eventName === 'add' || eventName === 'remove' || eventName === 'reset' ) {
 			var dit = this, args = arguments;
+			
+			if (eventName === 'add') {
+				var toBeCloned = args;
+				args = [];
+				_.each(toBeCloned, function(arg, index) {
+					if (index === 3) {
+						// these are the options containing the index a model element was added at.
+						var clonedArg = _.clone(arg);
+						args.push(clonedArg);
+					} else {
+						args.push(arg);
+					}
+				});
+			}
+			
 			Backbone.Relational.eventQueue.add( function() {
 					trigger.apply( dit, args );
 				});
