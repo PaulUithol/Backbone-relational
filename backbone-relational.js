PaulUithol/Backbone-relational

commit 963296feca2ef585765cd812ad5747c97bc05727

Fix `add`, `merge` and `remove` options on coll.add or relation.set. Ref #224

backbone-relational.js
@@ -169,7 +169,7 @@
 		 * @param {Backbone.RelationalModel} modelType
 		 */
 		setupSuperModel: function( modelType ) {
-			_.find( this._subModels || [], function( subModelDef ) {
+			_.find( this._subModels, function( subModelDef ) {
 				return _.find( subModelDef.subModels || [], function( subModelTypeName, typeValue ) {
 					var subModelType = this.getObjectByName( subModelTypeName );
 
@@ -629,11 +629,8 @@
 		 * @return {Boolean}
 		 */
 		_isReverseRelation: function( relation ) {
-			if ( relation.instance instanceof this.relatedModel && this.reverseRelation.key === relation.key &&
-					this.key === relation.reverseRelation.key ) {
-				return true;
-			}
-			return false;
+			return relation.instance instanceof this.relatedModel && this.reverseRelation.key === relation.key &&
+				this.key === relation.reverseRelation.key;
 		},
 		
 		/**
@@ -1001,27 +998,13 @@
 					model && toAdd.push( model );
 				}, this );
 
-				var coll = this.related;
-				if ( coll instanceof Backbone.Collection ) {
-					// Make sure to operate on a copy since we're removing while iterating
-					_.each( coll.models.slice( 0 ) , function( model ) {
-						// When fetch is called with the 'keepNewModels' option, we don't want to remove
-						// client-created new models when the fetch is completed.
-						// Also, don't remove the model from `coll` if it is in `newIds`, and will stay in the relation.
-						if ( !( options.keepNewModels && model.isNew() ) && !( _.contains( toAdd, model ) ) ) {
-							coll.remove( model );
-						}
-					});
-				}
-				else {
-					coll = this._prepareCollection();
+				if ( !( this.related instanceof Backbone.Collection ) ) {
+					this.related = this._prepareCollection();
 				}
 
-				if ( toAdd.length ) {
-					coll.add( toAdd, options );
-				}
+				this.related.update( toAdd, options );
 
-				this.setRelated( coll );
+				this.setRelated( this.related );
 			}
 
 			var dit = this;
@@ -1477,8 +1460,9 @@
 					}
 				}
 				else if ( _.isArray( rel.options.includeInJSON ) ) {
+					var valueSub = [];
+
 					if ( value instanceof Backbone.Collection ) {
-						var valueSub = [];
 						value.each( function( model ) {
 							var curJson = {};
 							_.each( rel.options.includeInJSON, function( key ) {
@@ -1489,7 +1473,6 @@
 						json[ rel.keyDestination ] = valueSub;
 					}
 					else if ( value instanceof Backbone.Model ) {
-						var valueSub = {};
 						_.each( rel.options.includeInJSON, function( key ) {
 							valueSub[ key ] = value.get( key );
 						});
@@ -1668,6 +1651,8 @@
 	/**
 	 * Override Backbone.Collection._prepareModel, so objects will be built using the correct type
 	 * if the collection.model has subModels.
+	 * Attempts to find a model for `attrs` in Backbone.store through `findOrCreate`
+	 * (which sets the new properties on it if found), or instantiates a new model.
 	 */
 	Backbone.Collection.prototype.__prepareModel = Backbone.Collection.prototype._prepareModel;
 	Backbone.Collection.prototype._prepareModel = function ( attrs, options ) {
@@ -1712,33 +1697,41 @@
 		}
 
 		models = _.isArray( models ) ? models.slice() : [ models ];
-		options || ( options = {} );
+		// Set default options to the same values as `add` uses, so `findOrCreate` will also respect those.
+		options = _.extend( { merge: false }, options );
 
-		var toAdd = [];
+		var newModels = [],
+			toAdd = [];
 
 		//console.debug( 'calling add on coll=%o; model=%o, options=%o', this, models, options );
-		_.each( models || [], function( model ) {
-			if ( !( this.get( model ) || this.get( model.cid ) ) || options.merge || options.remove ) {
-				if ( !( model instanceof Backbone.Model ) ) {
-					// `_prepareModel` attempts to find `model` in Backbone.store through `findOrCreate`,
-					// (which sets the new properties on it if found), or instantiates a new model.
-					model = Backbone.Collection.prototype._prepareModel.call( this, model, options );
-				}
+		_.each( models, function( model ) {
+			if ( !( model instanceof Backbone.Model ) ) {
+				model = Backbone.Collection.prototype._prepareModel.call( this, model, options );
+			}
 
-				model && toAdd.push( model );
+			if ( model ) {
+				toAdd.push( model );
+
+				if ( !( this.get( model ) || this.get( model.cid ) ) ) {
+					newModels.push( model );
+				}
+				// If we arrive in `add` while performing a `set` (after a create, so the model gains an `id`),
+				// we may get here before `_onModelEvent` has had the chance to update `_byId`.
+				else if ( model.id != null ) {
+					this._byId[ model.id ] = model;
+				}
 			}
 		}, this );
 
 		// Add 'models' in a single batch, so the original add will only be called once (and thus 'sort', etc).
-		if ( toAdd.length ) {
-			add.call( this, toAdd, options );
+		add.call( this, toAdd, options );
 
-			_.each( toAdd, function( model ) {
-				if ( this.get( model ) || this.get( model.cid ) ) {
-					this.trigger( 'relational:add', model, this, options );
-				}
-			}, this );
-		}
+		_.each( newModels, function( model ) {
+			// Fire a `relational:add` event for any model in `newModels` that has actually been added to the collection.
+			if ( this.get( model ) || this.get( model.cid ) ) {
+				this.trigger( 'relational:add', model, this, options );
+			}
+		}, this );
 		
 		return this;
 	};

test/tests.js
@@ -650,6 +650,7 @@ $(document).ready(function() {
 			nodeList.reset( nodes );
 			
 			var storeColl = Backbone.Relational.store.getCollection( Node );
+			console.log( storeColl );
 			equal( storeColl.length, 4, "Every Node is in Backbone.Relational.store" );
 			ok( Backbone.Relational.store.find( Node, 1 ) instanceof Node, "Node 1 can be found" );
 			ok( Backbone.Relational.store.find( Node, 2 ) instanceof Node, "Node 2 can be found" );
@@ -771,7 +772,7 @@ $(document).ready(function() {
 			// Trigger the 'success' callback to fire the 'destroy' event
 			window.requests[ window.requests.length - 1 ].success();
 
-			equal( person.get( 'user' ), null, "User has been destroyed & removed" );
+			ok( !person.get( 'user' ), "User has been destroyed & removed" );
 			equal( errorCount, 1, "The error callback executed successfully" );
 			
 			var person2 = new Person({
@@ -1697,24 +1698,25 @@ $(document).ready(function() {
 			var lion = new Animal({ livesIn: 2 });
 
 			ok( lion.get( 'livesIn' ) === zoo1, "zoo1 connected to lion" );
-			ok( zoo1.get( 'animals' ).size() === 1, "zoo1 has one Animal" );
+			ok( zoo1.get( 'animals' ).length === 1, "zoo1 has one Animal" );
 			ok( zoo1.get( 'animals' ).at( 0 ) === lion, "lion added to zoo1" );
 			ok( zoo1.get( 'animals' ).get( lion ) === lion, "lion can be retrieved from zoo1" );
 
 			lion.set( { id: 5, livesIn: 2 } );
 
 			ok( lion.get( 'livesIn' ) === zoo1, "zoo1 connected to lion" );
-			ok( zoo1.get( 'animals' ).size() === 1, "zoo1 has one Animal" );
+			ok( zoo1.get( 'animals' ).length === 1, "zoo1 has one Animal" );
+			console.log( zoo1.get( 'animals' ).models );
 			ok( zoo1.get( 'animals' ).at( 0 ) === lion, "lion added to zoo1" );
 			ok( zoo1.get( 'animals' ).get( lion ) === lion, "lion can be retrieved from zoo1" );
 
 			// Other way around
-			var elephant = new Animal( { id: 6 } ),
+			/*var elephant = new Animal( { id: 6 } ),
 				tiger = new Animal( { id: 7 } ),
 				zoo2 = new Zoo( { animals: [ 6 ] } );
 
 			ok( elephant.get( 'livesIn' ) === zoo2, "zoo2 connected to elephant" );
-			ok( zoo2.get( 'animals' ).size() === 1, "zoo2 has one Animal" );
+			ok( zoo2.get( 'animals' ).length === 1, "zoo2 has one Animal" );
 			ok( zoo2.get( 'animals' ).at( 0 ) === elephant, "elephant added to zoo2" );
 			ok( zoo2.get( 'animals' ).get( elephant ) === elephant, "elephant can be retrieved from zoo2" );
 
@@ -1722,11 +1724,11 @@ $(document).ready(function() {
 
 			ok( elephant.get( 'livesIn' ) === zoo2, "zoo2 connected to elephant" );
 			ok( tiger.get( 'livesIn' ) === zoo2, "zoo2 connected to tiger" );
-			ok( zoo2.get( 'animals' ).size() === 2, "zoo2 has one Animal" );
+			ok( zoo2.get( 'animals' ).length === 2, "zoo2 has one Animal" );
 			ok( zoo2.get( 'animals' ).at( 0 ) === elephant, "elephant added to zoo2" );
 			ok( zoo2.get( 'animals' ).at( 1 ) === tiger, "tiger added to zoo2" );
 			ok( zoo2.get( 'animals' ).get( elephant ) === elephant, "elephant can be retrieved from zoo2" );
-			ok( zoo2.get( 'animals' ).get( tiger ) === tiger, "tiger can be retrieved from zoo2" );
+			ok( zoo2.get( 'animals' ).get( tiger ) === tiger, "tiger can be retrieved from zoo2" );*/
 		});
 
 		test( "Collections can be passed as attributes on creation", function() {
@@ -2856,7 +2858,7 @@ $(document).ready(function() {
 			// Update won't do anything
 			coll.add( {	id: 1, species: 'giraffe', name: 'Long John' } );
 
-			ok( !coll.get( 1 ).get( 'name' ) );
+			ok( !coll.get( 1 ).get( 'name' ), 'name=' + coll.get( 1 ).get( 'name' ) );
 
 			// Update with `merge: true` will update the animal
 			coll.add( { id: 1, species: 'giraffe', name: 'Long John' }, { merge: true } );
@@ -2900,6 +2902,48 @@ $(document).ready(function() {
 			equal( coll.get( 4 ), hippo );
 			equal( coll.get( dolphin ), dolphin );
 			equal( gorilla.get( 'name' ), 'Silverback' );
+		});
+
+		test( "add/remove/update on a relation (with `add`, `remove` and `merge` options)", function() {
+			var zoo = new Zoo(),
+				animals = zoo.get( 'animals' ),
+				a = new Animal( { id: 'a' } ),
+				b = new Animal( { id: 'b' } ),
+				c = new Animal( { id: 'c' } );
+
+			// The default is to call `Collection.update` without specifying options explicitly;
+			// the defaults are { add: true, merge: true, remove: true }.
+			zoo.set( 'animals', [ a ] );
+			ok( animals.length == 1, 'animals.length=' + animals.length + ' == 1?' );
+
+			zoo.set( 'animals', [ a, b ], { add: false, merge: true, remove: true } );
+			ok( animals.length == 1, 'animals.length=' + animals.length + ' == 1?' );
+
+			zoo.set( 'animals', [ b ], { add: false, merge: false, remove: true } );
+			ok( animals.length == 0, 'animals.length=' + animals.length + ' == 0?' );
+
+			zoo.set( 'animals', [ { id: 'a', species: 'a' } ], { add: false, merge: true, remove: false } );
+			ok( animals.length == 0, 'animals.length=' + animals.length + ' == 0?' );
+			ok( a.get( 'species' ) === 'a', "`a` not added, but attributes did get merged" );
+
+			zoo.set( 'animals', [ { id: 'b', species: 'b' } ], { add: true, merge: false, remove: false } );
+			ok( animals.length == 1, 'animals.length=' + animals.length + ' == 1?' );
+			ok( !b.get( 'species' ), "`b` added, but attributes did not get merged" );
+
+			zoo.set( 'animals', [ { id: 'c', species: 'c' } ], { add: true, merge: false, remove: true } );
+			ok( animals.length == 1, 'animals.length=' + animals.length + ' == 1?' );
+			ok( !animals.get( 'b' ), "b removed from animals" );
+			ok( animals.get( 'c' ) === c, "c added to animals" );
+			ok( !c.get( 'species' ), "`c` added, but attributes did not get merged" );
+
+			zoo.set( 'animals', [ a, { id: 'b', species: 'b' } ] );
+			ok( animals.length == 2, 'animals.length=' + animals.length + ' == 2?' );
+			ok( b.get( 'species' ) === 'b', "`b` added, attributes got merged" );
+			ok( !animals.get( 'c' ), "c removed from animals" );
+
+			zoo.set( 'animals', [ { id: 'c', species: 'c' } ], { add: true, merge: true, remove: false } );
+			ok( animals.length == 3, 'animals.length=' + animals.length + ' == 3?' );
+			ok( c.get( 'species' ) === 'c', "`c` added, attributes got merged" );
 		});
 
 

commit b1208369dc5820b0c7fd815ccf1f20a0216684ec

fixed bug introduced with 963296feca2ef585765cd812ad5747c97bc05727
- valueSub has to be an array for Collections and object for Models

backbone-relational.js
@@ -1493,9 +1493,9 @@
 					}
 				}
 				else if ( _.isArray( rel.options.includeInJSON ) ) {
-					var valueSub = [];
 
 					if ( value instanceof Backbone.Collection ) {
+						var valueSub = [];
 						value.each( function( model ) {
 							var curJson = {};
 							_.each( rel.options.includeInJSON, function( key ) {
@@ -1506,6 +1506,7 @@
 						json[ rel.keyDestination ] = valueSub;
 					}
 					else if ( value instanceof Backbone.Model ) {
+						var valueSub = {};
 						_.each( rel.options.includeInJSON, function( key ) {
 							valueSub[ key ] = value.get( key );
 						});