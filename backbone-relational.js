a7634e7d9deac64e3da455a3fde13b96ae253612
Paul Uithol <paul.uithol@gmail.com>
2015-08-19 16:04:07 +0300

    Update versions to 0.10.0

--- backbone-relational.js
+++ backbone-relational.js
@@ -1,6 +1,6 @@
 /* vim: set tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab: */
 /**
- * Backbone-relational.js 0.10.0-beta.0
+ * Backbone-relational.js 0.10.0
  * (c) 2011-2014 Paul Uithol and contributors (https://github.com/PaulUithol/Backbone-relational/graphs/contributors)
  *
  * Backbone-relational may be freely distributed under the MIT license; see the accompanying LICENSE.txt.
